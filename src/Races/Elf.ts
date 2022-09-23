529d893b34842d733a3b120ebe045e11592f0443
Gisele <gisele.viedenhelfen@gmail.com>
2022-09-22 21:06:52 -0300

    habilita as raças

---
+++ src/Races/Elf.ts
@@ -0,0 +1,16 @@
+import Race from './Race';
+
+class Elf extends Race {
+  private _maxLifePoints = 99;
+  private static _raceInstance = 0;
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    this._raceInstance += 1;
+    return Elf._raceInstance;
+  }
+}
+export default Elf;
---
+++ src/Races/Orc.ts
@@ -0,0 +1,16 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints = 74;
+  private static _raceInstance = 0;
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    this._raceInstance += 1;
+    return Orc._raceInstance;
+  }
+}
+export default Orc;
